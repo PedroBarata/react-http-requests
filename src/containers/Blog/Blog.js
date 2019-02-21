f2f6ff8dd3e88708c4036c34d46e8e942fc27dd5
pedrobarata <pedrofelipebarata@gmail.com>
2019-02-21 16:04:21 -0300

    feat(9.189): Makin a post Selectable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import axios from "axios";
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
   componentDidMount() {
@@ -25,9 +26,18 @@ class Blog extends Component {
     });
   }
 
+  selectedPostHandler = (id) => {
+    this.setState({selectedPostId: id});
+  }
+
   render() {
       const posts = this.state.posts.map(post => {
-        return (<Post key={post.id} title={post.title} author={post.author}/>);
+        return (<Post 
+            key={post.id} 
+            title={post.title} 
+            author={post.author}
+            clicked={() => this.selectedPostHandler(post.id)}
+            />);
       });
     return (
       <div>
@@ -35,7 +45,7 @@ class Blog extends Component {
           {posts}
         </section>
         <section>
-          <FullPost />
+          <FullPost selectedPostId={this.state.selectedPostId}/>
         </section>
         <section>
           <NewPost />
