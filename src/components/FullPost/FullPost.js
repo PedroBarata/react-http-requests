47b94a9443a57b8e3b5f6eaeda026aad949034ad
pedrobarata <pedrofelipebarata@gmail.com>
2019-02-21 16:21:18 -0300

    feat(9.190) Fetching Data on Update (without Creating Infinite Loops)

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,24 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './FullPost.css';
+import "./FullPost.css";
+import axios from "axios";
 
 class FullPost extends Component {
-    render () {
-        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
-       if(this.props.selectedPostId) {
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+  state = {
+    loadedPost: null
+  };
 
-        );
-       }
-        return post;
+  componentDidUpdate() {
+    if (this.props.selectedPostId) {
+      if (
+        !this.state.loadedPost ||
+        (this.state.loadedPost &&
+          this.state.loadedPost.id !== this.props.selectedPostId)
+      ) {
+        axios
+          .get(
+            "https://jsonplaceholder.typicode.com/posts/" +
+              this.props.selectedPostId
+          )
+          .then(response => {
+            this.setState({ loadedPost: response.data });
+          });
+      }
     }
+  }
+
+  render() {
+    let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+    if (this.props.selectedPostId) {
+      post = <p style={{ textAlign: "center" }}>Loading...!</p>;
+    }
+    if (this.state.loadedPost) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}</p>
+          <div className="Edit">
+            <button className="Delete">Delete</button>
+          </div>
+        </div>
+      );
+    }
+    return post;
+  }
 }
 
 export default FullPost;
