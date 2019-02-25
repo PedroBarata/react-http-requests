75ec3b3d5ba9f65bd5f5bed5b50e8df1186ecf8b
pedrobarata <pedrofelipebarata@gmail.com>
2019-02-25 13:50:31 -0300

    feat(11.226): Passing Route Parameters

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Posts from '../Posts/Posts';
 import { Route, NavLink } from 'react-router-dom';
 import NewPost from '../NewPost/NewPost';
+import FullPost from '../FullPost/FullPost';
 import "./Blog.css";
 
 class Blog extends Component {
@@ -33,6 +34,8 @@ class Blog extends Component {
 
         <Route path="/" exact component={Posts} />
         <Route path="/new-post" component={NewPost} />
+        <Route path="/:id" component={FullPost} />
+
       </div>
     );
   }
